6c077734489faaacf75847a7db561b24dab7658f
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2026-02-26 15:53:52 +0600

    First commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      // Disable the rule so explicit `any` is allowed
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,39 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactCompiler: true,
+
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/:path*",
+        destination: "https://bengal-bazar-server.vercel.app/:path*",
+      },
+    ];
+  },
+
+  experimental: {
+    optimizePackageImports: [
+      "lodash",
+      "gsap",
+      "@gsap/react",
+      "react-toastify",
+      "@iconify/react",
+    ],
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 };
 
 export default nextConfig;
