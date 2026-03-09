d7d403b38c5100c6add76bd4ad0c833e8c035826
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2026-03-09 15:53:59 +0600

    Rewrite redux store.ts

--- src/libs/redux/store.ts
+++ src/libs/redux/store.ts
@@ -1,22 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import backdropReducer from "./features/backdrop/backdropSlice";
+
 import productQuickViewReducer from "./features/productQuickView/productQuickViewSlice";
 import { baseApiSlice } from "./apiSlices/baseApiSlice";
 import categoriesReducer from "./features/categories/categoriesSlice";
-
-import returnRequestReducer from "./features/returnRequest/returnRequest";
 import customerReducer from "./features/customer/customerSlice";
-import analyticsReducer from "./features/analytics/analyticsSlice";
 
 export const makeStore = () =>
   configureStore({
     reducer: {
-      backdrop: backdropReducer,
       productQuickView: productQuickViewReducer,
       categories: categoriesReducer,
-      returnRequest: returnRequestReducer,
       customer: customerReducer,
-      analytics: analyticsReducer,
+
       [baseApiSlice.reducerPath]: baseApiSlice.reducer,
     },
     middleware: (getDefaultMiddleware) =>
