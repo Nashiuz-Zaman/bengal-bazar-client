408c3debe5a9a5fb1c21ceaaad64d06e16245615
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2026-03-09 15:56:52 +0600

    Refactor

--- src/libs/redux/store.ts
+++ src/libs/redux/store.ts
@@ -1,17 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import productQuickViewReducer from "./features/productQuickView/productQuickViewSlice";
 import { baseApiSlice } from "./apiSlices/baseApiSlice";
-import categoriesReducer from "./features/categories/categoriesSlice";
-import customerReducer from "./features/customer/customerSlice";
 
 export const makeStore = () =>
   configureStore({
     reducer: {
-      productQuickView: productQuickViewReducer,
-      categories: categoriesReducer,
-      customer: customerReducer,
-
       [baseApiSlice.reducerPath]: baseApiSlice.reducer,
     },
     middleware: (getDefaultMiddleware) =>
