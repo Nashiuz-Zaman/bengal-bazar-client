025d58a62a4ce9b9e39b1c89bc95b439b35d43ae
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2026-06-01 03:47:43 +0600

    feat(common): add hooks

---
+++ src/common/hooks/ui/useDynamicHeight.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { RefObject, useMemo } from "react";
+import { useResizeObserver } from "../../useResizeObserver";
+import { getHeight } from "@/utils/getHeight";
+import { useScreenSize } from "../../useScreenSize";
+
+interface IUseDynamicHeightOptions {
+  refElements?: (RefObject<HTMLElement> | null)[];
+  fixedHeights?: number[];
+}
+
+export const useDynamicHeight = ({
+  refElements = [],
+  fixedHeights = [],
+}: IUseDynamicHeightOptions) => {
+  const { height: screenHeight } = useScreenSize();
+  const entries = useResizeObserver(refElements);
+
+  const dynamicHeight = useMemo(() => {
+    if (!screenHeight || entries.includes(null)) return null;
+
+    const refsHeight = entries
+      .filter((entry): entry is ResizeObserverEntry => entry !== null)
+      .reduce((sum, entry) => sum + getHeight(entry), 0);
+
+    const fixed = fixedHeights.reduce((sum, h) => sum + h, 0);
+
+    return screenHeight - refsHeight - fixed;
+  }, [screenHeight, entries, fixedHeights]);
+
+  return dynamicHeight;
+};
