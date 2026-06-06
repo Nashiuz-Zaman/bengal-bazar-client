75d94ddf8a4c2ae33ce260694ede002f27918ee6
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2026-06-06 17:56:45 +0600

    refactor(codebase): remove constants folder

--- src/constants/address.ts
+++
@@ -1,6 +0,0 @@
-export const AddressType = Object.freeze({
-  SHIPPING: "SHIPPING",
-  BILLING: "BILLING",
-} as const);
-
-export type TAddressType = (typeof AddressType)[keyof typeof AddressType];
